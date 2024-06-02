f2f7958199273648bf662cf37b89bac8992f4074
Sushant Thapa <jungthapasushant@gmail.com>
2024-06-02 15:56:27 +0545

    added a welcome screen

--- surial.c
+++ surial.c
@@ -7,13 +7,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
-
 #include <string.h>
 #include <sys/ioctl.h>
 
 
 /*** defines***/
 
+#define SURIAL_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 
@@ -120,7 +120,16 @@ void abFree(struct abuf *ab){
 void editorDrawRows(struct abuf *ab){
   int y;
   for(y=0; y<E.screenrows;y++){
+
+    if (y == E.screenrows / 3) {
+      char welcome[30];
+      int welcomelen = snprintf(welcome, sizeof(welcome), "Surial Editor --version %s", SURIAL_VERSION);
+      if (welcomelen > E.screencolumns) welcomelen = E.screencolumns;
+      abAppend(ab, welcome, welcomelen);
+    } else {
+
     abAppend(ab, "~", 1);
+    }
     abAppend(ab, "\x1b[K", 3);
 if( y< E.screenrows -1){
     abAppend(ab, "\r\n", 2);
